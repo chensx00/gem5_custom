46cd6eddee119922fe939b96a973bbc636d25af1
AYANAMI <869066469@qq.com>
2024-01-03 18:29:13 +0800

    加入verilator侧的代码

---
+++ ext/custom/model_counter/custom_packet_counter.hh
@@ -0,0 +1,22 @@
+#ifndef __CUSTOM_PACKET_COUNTER_HH__
+#define __CUSTOM_PACKET_COUNTER_HH__
+
+
+#include <cstdlib>
+#include <iostream>
+
+struct inputCounter
+{
+    uint64_t    ena;
+    uint64_t    rst;
+};
+
+struct outputCounter 
+{
+    uint64_t    out;
+};
+
+
+
+
+#endif
